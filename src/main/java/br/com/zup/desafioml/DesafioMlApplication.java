c1cdffc02f83d0833a2f83f16245632377b446b7
Pedro <pedro.diniz@zup.com.br>
2021-08-03 19:52:06 -0300

    Primeiro commit, importação do projeto criado pelo Spring Initializr

---
+++ src/main/java/br/com/zup/desafioml/DesafioMlApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafioml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/desafioml/DesafioMlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.desafioml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
